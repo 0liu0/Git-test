a500ced729f03374d5b81cbc6b66067edc24733e
0liu0 <100211747+0liu0@users.noreply.github.com>
2023-01-27 23:40:58 +0800

    Update GitTest.java

--- git/src/main/java/com/liuche/git/GitTest.java
+++ git/src/main/java/com/liuche/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("大家好！我是刘帅帅");
         System.out.println("第二次提交");
         System.out.println("正在夜以继日实现代码");
+        System.out.println("这个功能有点儿难实现");
     }
 }
