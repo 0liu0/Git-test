0ee75f20916303b5add8df61d2e79ceeeeab6056
liuche <706716852@qq.com>
2023-01-27 23:25:02 +0800

    idea第三次提交

--- git/src/main/java/com/liuche/git/GitTest.java
+++ git/src/main/java/com/liuche/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("大家好！我是刘帅帅");
         System.out.println("大帅帅第二次提交");
+        System.out.println("大帅帅第三次提交");
     }
 }
