a99cc1aabcc74caa3e15ca844f0c8ed8fa6ad2a3
liuche <706716852@qq.com>
2023-01-27 23:23:58 +0800

    idea第二次提交

--- git/src/main/java/com/liuche/git/GitTest.java
+++ git/src/main/java/com/liuche/git/GitTest.java
@@ -3,5 +3,6 @@ package com.liuche.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("大家好！我是刘帅帅");
+        System.out.println("大帅帅第二次提交");
     }
 }
